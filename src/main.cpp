7a94b230313da89e4347dbe5cea27c62f17ede5b
stephane <darksh3ll.js@gmail.com>
2021-03-10 09:32:44 +0100

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+int age = 42;
+
+void setup()
+{
+  lcd.begin(16, 2);
+  lcd.print(age);
+}
+
+void loop()
+{
+  lcd.setCursor(0, 1);
+  lcd.print("stephane");
+}
